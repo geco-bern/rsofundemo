a0430234e996581cd9481c59855710de864281e2
FrancescoGrossi-unimi <115780963+FrancescoGrossi-unimi@users.noreply.github.com>
2024-02-15 08:40:34 +0100

    Add files via upload
    Run with two different sites to use the multisite_p_model vignette

---
+++ analysis/00_p_model_files_creation.R
@@ -0,0 +1,22 @@
+# create drivcers and validtion files used in p_model
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(reshape2)
+library(lubridate)
+library(FluxDataKit)
+library(ncdf4)
+library(rsofun)
+source("read_meta_fdk.R")
+source("generation_validation_bysitename.R")
+
+# select your own paths, the outpath doesn't need to exist since it will be created
+
+site <- "FR-Pue"
+csv_path <- paste0(getwd(),"/fluxdatakit_oct3/FLUXDATAKIT_FLUXNET/")
+lsm_path <- paste0(getwd(),"/fluxdatakit_oct3/FLUXDATAKIT_LSM/")
+nc_path <- paste0(getwd(),"/vignettes/ancillary_data/")
+out_path <- paste0(getwd(),"/fluxdatakit_oct3/FLUXDATAKIT_RDA/")
+
+# running once per site since the results are saved 
+create_driver_validation(site,csv_path,lsm_path,nc_path,out_path)
