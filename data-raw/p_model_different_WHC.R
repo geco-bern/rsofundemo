cf21a4eac091f2b2c90dc9b5fa057fdd37b6ca5d
FrancescoGrossi-unimi <115780963+FrancescoGrossi-unimi@users.noreply.github.com>
2024-05-23 12:02:19 +0200

    Update p_model_different_WHC.R

--- data-raw/p_model_different_WHC.R
+++ data-raw/p_model_different_WHC.R
@@ -6,6 +6,11 @@ library(ncdf4)
 library(cwd)
 library(FluxDataKit)
 
+# We use `rsofun_driver_data_v3.rds`, provided on Zenodo and whc 2m
+# driver data can be found on [Zenodo](https://zenodo.org/records/10885934)
+# Download that files and specify its local path
+cost_whc_driver <- var_whc_driver <- readRDS(here("data","rsofun_driver_data_v3.rds")) #insert your local path
+
 # function used to create dataframes
 get_annual_aet_pet <- function(df){
   df |>
@@ -62,7 +67,6 @@ params_modl <- list(
   kc_jmax            = 0.41
 )
 
-cost_whc_driver <- var_whc_driver <- readRDS(here("data","filtered_driver.rds"))
 
 # change whc to previous result
 
@@ -241,8 +245,5 @@ keep <- fdk_site_fullyearsequence |>
 cost_whc_adf <- cost_whc_adf[which(cost_whc_adf$sitename %in% keep$sitename),]
 var_whc_adf <- var_whc_adf[which(var_whc_adf$sitename %in% keep$sitename),]
 
-# the ystart and yend are already correct
-
-
-saveRDS(cost_whc_adf,here("data","costant_whc.rds"))
-saveRDS(var_whc_adf,here("data","variable_whc.rds"))
+saveRDS(cost_whc_adf,here("data","output_costant_whc.rds"))
+saveRDS(var_whc_adf,here("data","output_variable_whc.rds"))
